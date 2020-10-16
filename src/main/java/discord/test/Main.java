02940c65ac35bcea688b32e7829f78efd5dd6430
Vladislav <grifon7676@gmail.com>
2020-10-16 05:17:30 +0300

    Added support for mac/linux. Program will now describe standard errors.

--- src/main/java/discord/test/Main.java
+++ src/main/java/discord/test/Main.java
@@ -10,32 +10,36 @@ import java.net.URL;
 import java.util.Arrays;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Main {
-	public static void main(String[] args) throws LoginException, InterruptedException {
+	public static void main(String[] args) throws InterruptedException {
 		OSValidator os = new OSValidator();
-		
-		
 		Scanner scanner;
 		String currentDirectory = System.getProperty("user.dir");
 		File file;
-		if (os.getUserOS() == 2 || os.getUserOS() == 3)
+		
+		if (os.getUserOS() == 2 || os.getUserOS() == 3) {
+			System.out.println("Starting on linux/mac");
 			file = new File(currentDirectory + "/token.txt");
+		}
 		else {
 			if (os.getUserOS() != 1)
-				System.out.println("Can not figure out what OS you are using. Bot do not support Solaris. \nProgram will continue, assuming os is windows");
+				System.err.println("Can not figure out what OS you are using. Bot do not support Solaris. \nProgram will try to start, assuming that your OS is Windows.");
+			System.out.println("Starting on Windows");
 			file = new File(currentDirectory + "\\token.txt");
 		}
 		try {
 			scanner = new Scanner(new FileInputStream(file));
-			new Bot(scanner.nextLine());
+			String[] strings = scanner.nextLine().strip().split("[\\s=]+");
+			new Bot(strings[strings.length-1]);
 		} catch (FileNotFoundException | NoSuchElementException e) {
-			System.err.println(Arrays.toString(e.getStackTrace()));
-			System.err.println("Error: file token.txt not found");
+			System.err.println("Error: File token.txt not found.");
 			System.exit(0);
 		}
-
+		catch (LoginException e) {
+			System.err.println("Error: Login Exception. Your bot token isn't working. Check if file token.txt contains your bot token.");
+			System.exit(0);
+		}
 //		or this
 //
 //		String token="your token here";
