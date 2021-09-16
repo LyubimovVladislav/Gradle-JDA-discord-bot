e8fa2078e65e3ee57f34e9965991eb784656a955
Vladislav Lyubimov <43727166+LyubimovVladislav@users.noreply.github.com>
2021-09-16 19:31:55 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-git pull
+git pull https://github.com/LyubimovVladislav/Gradle-JDA-discord-bot.git master
+echo Starting...
 gradle run
 echo Press any key to continue...
 read -p "Press [Enter] to exit ..."
